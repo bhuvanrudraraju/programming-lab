9e2e52b9a66be28cb73803a6b5619a5ad7af4240
Bhuvan Rudraraju <bhuvanrudraraju08@gmail.com>
2026-09-21 18:15:06 +0530

    Remove online exam eligibility check program

--- c/online-exam-eligibility.c
+++ c/online-exam-eligibility.c
@@ -1,16 +1,1 @@
-#include<stdio.h>
-int main()
-{
-    int x;
-    printf("Enter Attendance Percentage ");
-    scanf("%d",&x);
-    if(x<0 || x>100)
-        printf("Invalid Attendance Percentage");
-    else if( x>=75)
-        printf("Eligible for examination");
-    else if(x>=65)
-        printf("Conditionally Eligible");
-    else 
-        printf("Not Eligible");
-    return 0;
-}
+
