825a8189707161d6ee5134237d0008424d92c245
Bhuvan Rudraraju <bhuvanrudraraju08@gmail.com>
2026-09-26 03:43:47 +0530

    Add user input product calculation program

---
+++ JAVA/acs#pdt-of-3-userinput.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public static void main(String args[])
+{
+  Scanner sc=new Scanner(System.in);
+  int a,b,c,d;
+  System.out.println("Enter 3 values");
+  a=sc.nextInt();
+  b=sc.nextInt();
+  c=sc.nextInt();
+  d=c*a*b;
+  System.out.println("Product = "+d);
+}
