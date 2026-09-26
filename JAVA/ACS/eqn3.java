253bb21a3055f1fef4f8b5734cc92da6e36761e5
Bhuvan Rudraraju <bhuvanrudraraju08@gmail.com>
2026-09-26 12:28:51 +0530

    Create eqn3.java

---
+++ JAVA/ACS/eqn3.java
@@ -0,0 +1,12 @@
+/* WAP to solve the equation (x+3)/6 - (2x+5)/3 , value of x=5 */
+import java.util.*;
+public class equation
+{
+  public static void main(String args[])
+  {
+    int x=5;
+    double a;
+    a=(x+3)/6.0-(2*x+5)/3.0;
+    System.out.println(a);
+  }
+}
